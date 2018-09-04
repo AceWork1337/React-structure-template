7170f27216a7bba9552ee18a9f592dff4d2281f0
Unknown <ace92.work@gmail.com>
2018-09-04 15:10:06 +0200

    first commit

---
+++ client/scripts/routes/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../styles/routes/App.scss';
+//import npm package
+//favicon
+// import Favicon from 'react-favicon';
+
+//import components and containers
+
+
+
+export default class App extends React.Component {
+    render() {
+      return (
+       <div style={{textAlign: 'center'}}>
+           {/* <Favicon url={require("../styles/assets/img/favicon/favicon.ico")}/> */}
+          Hello begginer
+        </div>
+      );
+    }
+  }
